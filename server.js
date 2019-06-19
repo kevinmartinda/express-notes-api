3d91a0248d9374c9a83230aca9bab3d082b0d51f
kevinmartinda <kevin36.k3@gmail.com>
2019-06-19 08:51:25 +0700

    a liitle change on middleware

--- server.js
+++ server.js
@@ -4,14 +4,28 @@ const express = require('express'),
 	  port = process.env.PORT || 3000,
 	  routes = require('./routes'),
 	  bodyParser = require('body-parser'),
-	  middleware = require('./middleware'),
-	  cors = require('cors')
+	  cors = require('cors'),
+	  moment = require('moment')
 
 //log middleware
-app.use(middleware)
+app.use((req, res, next) => {
+	console.log(`${req.method} ${req.url} : accessed on ${moment().format('MMMM Do YYYY, h:mm:ss a')}`)
+	next()
+})
 
 //cors middleware
-app.use(cors({origin: "http://localhost"}))
+const whitelist = ['http://localhost', 'http://localhost:4200']
+app.use(
+	cors({origin: (origin, callback) => {
+		console.log(origin)
+		if(whitelist.indexOf(origin) !== -1 || origin === undefined){
+			callback(null, true)
+		} else {
+			callback(new Error('Not allowed by server..'))
+		}
+	}
+	})
+)
 
 //body-parser middleware
 app.use(bodyParser.urlencoded({ extended: true }))
